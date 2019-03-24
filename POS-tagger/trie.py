6540c7f4cb09bf86097e90c17c246c5dffbfed5d
BOWOO JANG <lisabowoo@gmail.com>
2019-03-24 23:38:32 +0900

    Add node structure of Trie

--- POS-tagger/trie.py
+++ POS-tagger/trie.py
@@ -1,10 +1,33 @@
-class Node():
-    """
-    Trie node
-    """
+from types import StringType
+
+
+class Node(object):
+    # Node: basis of Trie
+    def __init__(self, entry):
+        # check if the entry is string type
+        try:
+            assert type(entry) is StringType
+            self.entry = entry
+        except:
+            raise TypeError("entry attribute is not string type")
+        self.child = None
+        self.is_leaf = False
+        self.sibling = []
 
     
 class Trie():
     """
     Python implementation of Trie data structure
     """
+    def __init__(self):
+        pass
+
+    def addNode(self, args):
+        pass
+
+    def insertWord(self, args):
+        pass
+
+    def findWord(self, args):
+        pass
+
