722768f47835875f99939dff83079515b950ea4d
BOWOO JANG <lisabowoo@gmail.com>
2019-03-25 04:45:49 +0900

    Make morph dictionary in Trie format

--- POS-tagger/trie.py
+++ POS-tagger/trie.py
@@ -1,33 +1,49 @@
-from types import StringType
-
-
-class Node(object):
-    # Node: basis of Trie
-    def __init__(self, entry):
+class TrieNode(object):
+    def __init__(self, entry, pos=list(), can_leaf=False):
         # check if the entry is string type
         try:
-            assert type(entry) is StringType
+            assert isinstance(entry, str)
             self.entry = entry
         except:
             raise TypeError("entry attribute is not string type")
-        self.child = None
-        self.is_leaf = False
-        self.sibling = []
+        self.children = {}
+        self.can_leaf = can_leaf
+        self.pos = pos
+
+    def __repr__(self):
+        return repr(self.children if self.children else "Leaf")
+
+    def __eq__(self, char):
+        assert len(char) == 1, "Invalid character"
+        return self.entry == char
+
+    def add(self, morph=None, pos=None):
+        node = self.children.setdefault(morph[0], TrieNode(morph[0]))
+        if len(morph) == 1:
+            node.can_leaf = True
+            if pos not in node.pos:
+                node.pos.append(pos)
+        else:
+            node.add(morph[1:], pos)
 
     
-class Trie():
+class Trie(object):
     """
     Python implementation of Trie data structure
     """
     def __init__(self):
-        pass
+        self.trie = {}
 
-    def addNode(self, args):
-        pass
+    def __repr__(self):
+        return repr(self.trie)
 
-    def insertWord(self, args):
-        pass
+    def add(self, morph=None, pos=None):
+        node = self.trie.setdefault(morph[0], TrieNode(morph[0]))
+        if len(morph) == 1:
+            node.can_leaf = True
+            if pos not in node.pos:
+                node.pos.append(pos)
+        else:
+            node.add(morph[1:], pos)
 
-    def findWord(self, args):
-        pass
 
