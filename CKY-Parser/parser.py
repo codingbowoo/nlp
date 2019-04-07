d2d72aef5a0652685b6ca7c41af2c367d7302590
BOWOO JANG <lisabowoo@gmail.com>
2019-04-08 04:11:03 +0900

    define class Node

--- CKY-Parser/parser.py
+++ CKY-Parser/parser.py
@@ -1,10 +1,13 @@
 class Node(object):
-    def __init__(self.data):
-        self.data = data
-        self.left = None
-        self.right = None
+    def __init__(self, symbol, left=None, right=None):
+        self.symbol = symbol
+        self.left = left
+        self.right = right
 
+    def __repr__(self):
+        return self.symbol
 
 class ParseTree(object):
     def __init__(self):
         self.root = None
+
